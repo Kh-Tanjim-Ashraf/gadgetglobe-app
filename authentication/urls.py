2fe360eee84189f378fdfe735137277018ef3a19
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2026-06-30 13:12:05 +0600

    Feat: Implemented customer-registration; Redirect authenticated users from accessing login/registration page by manually typing in the browser

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from authentication.views import register
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='authentication/login.html'), name='login'),
+    path('login/', LoginView.as_view(
+            template_name='authentication/login.html', 
+            redirect_authenticated_user=True
+        ), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', view=register, name='register'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+
+def register(request):
+    # Redirect authenticated user to dashboard
+    if request.user.is_authenticated:
+        return redirect('secretPage')
+    
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    context = {'form': form}
+    return render(request, template_name='authentication/register.html', context=context)
